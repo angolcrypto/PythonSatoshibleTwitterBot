1b1462c034bd7ed8c99da1801d35f7ca6620bf59
saousername <samuelangeloliver@gmail.com>
2021-08-10 22:48:47 +0100

    all done

--- bot.py
+++ bot.py
@@ -2,10 +2,22 @@ import requests
 import time
 import web3
 import json
+import numpy as np
+import tweepy
+import shutil
 
 contract = ''
 slug = ''
 url = 'https://api.opensea.io/api/v1/events'
+
+consumer_key=''
+consumer_secret_key=''
+access_token=''
+access_token_secret=''
+
+auth=tweepy.OAuthHandler(consumer_key,consumer_secret_key)
+auth.set_access_token(access_token,access_token_secret)
+api=tweepy.API(auth)
 
 loop = True
 
@@ -15,10 +27,10 @@ def startBot():
     print('Created by @angolcrypto for Twitter')
     print('-- -- --')
     while loop == True:
-        print(checkSales())
+        checkSales()
         time.sleep(280)
 
-etherscan_template = 'https://api.etherscan.io/api?module=proxy&action=eth_getTransactionByHash&txhash=0x5bf25a7d8c5edf8e083a6fc8a46021768f8855f4ba996a6eac2a577a2688c76e&apikey=DT73NBDXHV6F8VYI4AWZD6BQUPN6MVATE1'
+etherscan_template = 'https://api.etherscan.io/api?module=proxy&action=eth_getTransactionByHash&txhash=0x5bf25a7d8c5edf8e083a6fc8a46021768f8855f4ba996a6eac2a577a2688c76e&apikey='
 
 def checkSales():
     # Variables
@@ -31,30 +43,31 @@ def checkSales():
     # Functions
     response = requests.request("GET", url, headers=headers, params=querystring)
     rj = json.loads(response.text)
-    print(rj)
 
     # Final Values
-    sales = []
     for sale in rj['asset_events']:
         _image = sale['asset']['image_url']
         _name = sale['asset']['name']
         _price = float(web3.Web3.fromWei(int(sale['total_price']),'ether'))
         _url = sale['asset']['permalink']
-        print('NEW SALE!')
-        print(_image)
-        print(_name)
-        print(_url)
-        print(str(_price))
-        sales.append({'image':_image,'name':_name,'price':_price,'url':_url})
 
-    print('--------------')
-    print('--------------')
-    print('Complete check!')
-    print('--------------')
-    print('--------------')
-    return sales
+        makeTweet(_name, _url, _image, _price)
 
-def makeTweet():
-    print('Tweet, tweet!')
+    print('Checked Sales')
+
+def makeTweet(name, url, image, price):
+    # Text only tweet:
+    # api.update_status('Testing the Developer API!')
+    text = f"{name} bought for {price}ETH 🚀 #nft #crypto #satoshibles #nfts"
+
+    response = requests.get(image, stream=True)
+    with open('img.png', 'wb') as out_file:
+        shutil.copyfileobj(response.raw, out_file)
+
+    del response
+
+    # API.update_with_media(filename[, status][, in_reply_to_status_id][, auto_populate_reply_metadata][, lat][, long][, source][, place_id][, file])
+    api.update_with_media('img.png', text)
+    print('Tweeted')
 
 startBot()
